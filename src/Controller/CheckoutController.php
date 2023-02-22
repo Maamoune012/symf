8179b5321e0a7c2686f2b45e2f787386eddab323
unconditional22 <kaurelie.w@gmail.com>
2023-02-22 16:01:24 +0000

    place order de index checkoutController

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CheckoutController extends AbstractController
 {
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/checkout', name: 'app_checkout')]
     public function index(): Response
     {
         return $this->render('checkout/index.html.twig', [
             'controller_name' => 'CheckoutController',
         ]);
+        
+        //Enregistrer mes produits OrderDetails()
+
+        /*foreach($cart->getFull() as $product){
+        }*/
+
     }
 }
--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StripeController extends AbstractController
 {
-    #[Route('/stripe/create-session', name: 'app_stripe_create_session')]
-    public function index()
+    #[Route('/commande/create-session', name: 'stripe_create_session')]
+    public function index(Product $cart)
     {
         #return $this->render('stripe/index.html.twig', [
         #'controller_name' => 'StripeController',
@@ -17,6 +19,21 @@ class StripeController extends AbstractController
 
         $products_for_stripe = [];
         $YOUR_DOMAIN = 'http://127.0.0.1:8000/';
+
+        foreach($cart as $product){
+
+            $products_for_stripe[] = [
+                'price_data' => [
+                    'currency' => 'eur',
+                    'price' => $product['product']->getPrice(),
+                    'product_data' => [
+                        'name' => $product['product']->getName(),
+                        'images' => [$YOUR_DOMAIN."/upload/".$product['product']->getPhotos],
+                    ],
+                ],
+                'quantity' => $product['quantity'],
+            ];
+        }
 
 
         \Stripe\Stripe::setApiKey('sk_test_51McVkAFVqpwutoMidXKkTjr8ZKBTzzKD3s27Cpn5GNeXEXG3yVkZuSiSF6198dAC6G05FcIA4BpcMqZHRAYjxGNa00rqLNYSQw');
@@ -29,6 +46,9 @@ class StripeController extends AbstractController
             'success_url' => $YOUR_DOMAIN . '/success.html',
             'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
         ]);
+
+        $response = new JsonResponse(['id' => $checkout_session->id]);
+        return $response;
 
     }
 }
