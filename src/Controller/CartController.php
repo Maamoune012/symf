66af49ca935726dd79b32a980074adf7607ae9d9
Willy Regit <yeyahsmaiga@gmail.com>
2023-03-01 16:36:00 +0000

    Panier: Afficher le Recap, Augmenter, reduire la quantite de produit, supprimer le produit

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,17 +2,59 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-    #[Route('/cart', name: 'app_cart')]
-    public function index(): Response
+
+  
+
+    #[Route('/panier', name: 'app_cart')]
+    public function index(Cart $cart)
     {
+        
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            'cart' => $cart->getFull()
         ]);
     }
+
+//ROUTE POUR AJOUTER UN ELEMENT AU PANIER
+    #[Route('/cart/add/{id}', name: 'add_to_cart')]
+    public function add(Cart $cart, $id)
+    {
+        $cart->add($id);
+        return $this->redirectToRoute('app_cart');
+    }
+
+//ROUTE POUR SUPPRIMER TOUUUT LE PANIER, test
+    #[Route('/cart/remove', name: 'remove_cart')]
+    public function remove(Cart $cart)
+    {
+        $cart->remove();
+        return $this->redirectToRoute('app_shop');
+    }
+
+
+    //ROUTE POUR SUPPRIMER UN ELEMENT DU PANIER, test
+    #[Route('/cart/delete/{id}', name: 'delete_from_cart')]
+    public function delete(Cart $cart, $id)
+    {
+        $cart->delete($id);
+        return $this->redirectToRoute('app_cart');
+    }
+
+    //ROUTE POUR REDUIRE LA QUANTITE D'UN ELEMENT DU PANIER, test
+    #[Route('/cart/decrease/{id}', name: 'decrease_from_cart')]
+    public function decrease(Cart $cart, $id)
+    {
+        $cart->decrease($id);
+        return $this->redirectToRoute('app_cart');
+    }
+    
 }
+
+
