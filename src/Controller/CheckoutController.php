33076ff58670450d44d4de3d1d27a8f06583dba8
unconditional22 <kaurelie.w@gmail.com>
2023-03-02 14:10:42 +0000

    ajout checkoutcontroller

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -2,28 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Order;
-use App\Entity\OrderDetails;
-use App\Form\CheckoutType;
-use DateTime;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-//use Symfony\Component\Form\Form;
-use Symfony\Entity\Form;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CheckoutController extends AbstractController
 {
+    #[Route('/checkout', name: 'app_checkout')]
     public function index()
+    {
         return $this->render('checkout/index.html.twig', [
-            //'controller_name' => 'CheckoutController',
-   
+            'controller_name' => 'CheckoutController',
         ]);
-
     }
-
-    //return $this->redirectToRoute(route: 'cart');
 }
-
-
